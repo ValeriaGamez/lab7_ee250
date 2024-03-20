ce17963fc4a048457b05f6bc4f99ccfb30ee16ab
ValeriaGamez <valeriagamezh.vg@gmail.com>
2024-03-20 15:29:25 -0700

    test_suite

--- test_suite.py
+++ test_suite.py
@@ -26,13 +26,13 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 while True: 
   time.sleep(0.5) 
 
-  # #test 1
-  # for i in range(0,4):
-  #   print('led 4 times');
-  #   GPIO.output(chan_list, GPIO.HIGH)
-  #   time.sleep(0.5)
-  #   GPIO.output(chan_list, GPIO.LOW)
-  #   time.sleep(0.5)
+  #test 1
+  for i in range(0,4):
+    print('led 4 times');
+    GPIO.output(chan_list, GPIO.HIGH)
+    time.sleep(0.5)
+    GPIO.output(chan_list, GPIO.LOW)
+    time.sleep(0.5)
 
   #test 2
   end = time.time() + 5
@@ -41,32 +41,32 @@ while True:
     light_val = mcp.read_adc(0)
     time.sleep(0.1)
     print(light_val)
-    # if light_val > light_thresh:
-    #   print("Bright")
-    # else:
-    #   print("Dark")
+    if light_val > light_thresh:
+      print("Bright")
+    else:
+      print("Dark")
   
   
-  # #test3
-  # for i in range(0,10):
-  #   print('led 10 times')
-  #   GPIO.output(chan_list, GPIO.HIGH)
-  #   time.sleep(0.2)
-  #   GPIO.output(chan_list, GPIO.LOW)
-  #   time.sleep(0.2)
+  #test3
+  for i in range(0,10):
+    print('led 10 times')
+    GPIO.output(chan_list, GPIO.HIGH)
+    time.sleep(0.2)
+    GPIO.output(chan_list, GPIO.LOW)
+    time.sleep(0.2)
 
-  # #test 4: 
-  # end = time.time() + 5
-  # while time.time() < end:
-  #   time.sleep(0.1)
-  #   sound_val = mcp.read_adc(1)
-  #   print(sound_val)
-  #   if sound_val > sound_thresh:
-  #     GPIO.output(chan_list, GPIO.HIGH)
-  #     time.sleep(0.1)
-  #   else:
-  #     GPIO.output(chan_list, GPIO.LOW)
-  #     time.sleep(0.1)
+  #test 4: 
+  end = time.time() + 5
+  while time.time() < end:
+    time.sleep(0.1)
+    sound_val = mcp.read_adc(1)
+    print(sound_val)
+    if sound_val > sound_thresh:
+      GPIO.output(chan_list, GPIO.HIGH)
+      time.sleep(0.1)
+    else:
+      GPIO.output(chan_list, GPIO.LOW)
+      time.sleep(0.1)
 
   
   
